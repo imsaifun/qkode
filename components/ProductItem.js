c6fa66b733e87bf3795b0ba3fa232d2f398cbaf9
imsaifun <youremail@yourdomain.comimsaifun@gmail.com>
2021-12-11 10:40:31 +0600

    working

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,14 +1,27 @@
-import { Thumbnail } from '@shopify/polaris';
-import React from 'react';
+import { ResourceList, Stack, TextStyle, Thumbnail } from "@shopify/polaris";
+import React from "react";
 
-const ProductItem = ({product}) => {
-    const image = product.images[0] ? product.images[0]?.originalSrc : null
-    return (
-        <>
-        <Thumbnail source={image}/>
-            <div>{product.title}</div>
-        </>
-    );
+const ProductItem = ({ product }) => {
+  console.log(product);
+  const image = product.images[0] ? product.images[0]?.originalSrc : null;
+  const media = <Thumbnail source={image} />;
+  const price = product.variants[0].price;
+  return (
+    <>
+      <ResourceList.Item media={media} id={product.id} accessibilityLabel={`View details for ${product.title}`}>
+        <Stack>
+          <Stack.Item>
+            <h4>
+              <TextStyle variation="strong">{product.title}</TextStyle>
+            </h4>
+          </Stack.Item>
+          <Stack.Item>
+            <p>${price}</p>
+          </Stack.Item>
+        </Stack>
+      </ResourceList.Item>
+    </>
+  );
 };
 
 export default ProductItem;
