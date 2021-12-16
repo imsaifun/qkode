c720f4adf6f881f2c07d7e645ff0ada1a8c8691b
imsaifun <youremail@yourdomain.comimsaifun@gmail.com>
2021-12-16 10:24:17 +0600

    working

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 import { ResourcePicker } from "@shopify/app-bridge-react";
-import { EmptyState, Page } from "@shopify/polaris";
-import { useState } from "react";
-import ProductList from "../components/ProductList";
+import { useEffect, useState } from "react";
+import ProductEmptyState from "../components/ProductEmptyState";
+import ProductPage from "../components/ProductPage";
 
 const index = () => {
 
     const [isOpen, setIsOpen] = useState(false)
     const [products, setProducts] = useState([])
+    const [productsId, setProductsId] = useState([])
 
+    useEffect(() => {
+        const ids = products.map(product => {
+            return {
+                id: product.id
+            }
+        });
+        setProductsId(ids)
+    }, [products])
     // console.log(products);
 
     const handleProductSelection = (payload) => {
@@ -21,29 +30,11 @@ const index = () => {
                 resourceType="Product"
                 open={isOpen}
                 onCancel={() => setIsOpen(false)}
-                onSelection={handleProductSelection} />
+                onSelection={handleProductSelection}
+                initialSelectionIds={productsId}
+            />
             {products.length > 0 ?
-                <Page
-                    title="Product Selector"
-                    primaryAction={
-                        {
-                            content: "Select product",
-                            onAction: () => setIsOpen(true)
-                        }
-                    }
-                >
-                    <ProductList products={products} />
-                </Page> : <EmptyState
-                    heading="Manage the products you want to display"
-                    action={{
-                        content: "Select products",
-                        onAction: () => setIsOpen(true),
-                    }}
-                    image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
-
-                >
-                    <p>Select the products you want to use on your banner</p>
-                </EmptyState>}
+                <ProductPage setIsOpen={setIsOpen} products={products} /> : <ProductEmptyState setIsOpen={setIsOpen} />}
         </>
     );
 };
